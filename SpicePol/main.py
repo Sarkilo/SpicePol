00314226d06133fbdfb0a79279fdd7068799f5bc
Sarkilo <87601983+Sarkilo@users.noreply.github.com>
2023-03-19 15:38:34 +0100

    Add files via upload
    Changed windows selection color
    Added all files to save and open 
    changed *,py to *.py in save

--- SpicePol/main.py
+++ SpicePol/main.py
@@ -3,12 +3,12 @@ from tkinter.filedialog import *
 import subprocess
 
 ide = Tk()
-icon = PhotoImage(file="SpicePol.png")
+icon = PhotoImage(file="images/SpicePol.png")
 background_color = "#353535"
 window_size = "640x480"
 text_color = "#ffffff"
 text_selection_background_color = "#0ac80a"
-window_highlight_color = "#10ff00"
+window_highlight_color = "#AB1951"
 cursor_color = "#ffffff"
 ide.title('SpicePol IDE')
 ide.iconphoto(True, icon)
@@ -25,7 +25,7 @@ def set_file_path(path):
 
 def save_as():
     if file_path == '':
-        path = asksaveasfilename(filetypes = [('Python files', '*.py')])
+        path = asksaveasfilename(filetypes = [('Python files', '*.py'), ('All files', '*.*')])
     else:
         path = file_path    
     with open(path, 'w') as f:
@@ -34,7 +34,7 @@ def save_as():
         set_file_path(path)
 
 def open_file():
-    path = askopenfilename(filetypes = [('Python files', '*.py')])
+    path = askopenfilename(filetypes = [('Python files', '*.py'), ('All files', '*.*')])
     with open(path, 'r') as f:
         code = f.read()
         editor.delete('1.0', END)
