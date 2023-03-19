6bde638b0bd9481c3d499188f5a30b1cfc9d20ad
xANtq <128253376+xANtq@users.noreply.github.com>
2023-03-19 21:04:53 +0100

    Update main.py

--- SpicePol/main.py
+++ SpicePol/main.py
@@ -3,12 +3,14 @@ from tkinter.filedialog import *
 import subprocess
 
 ide = Tk()
-icon = PhotoImage(file="images/SpicePol.png")
+icon = PhotoImage(file="SpicePol.png")
 background_color = "#353535"
+background_color_2 = "#43336F"
+background_color_menu_bar = "#3D3D60"
 window_size = "640x480"
 text_color = "#ffffff"
 text_selection_background_color = "#0ac80a"
-window_highlight_color = "#AB1951"
+window_highlight_color = "#10ff00"
 cursor_color = "#ffffff"
 ide.title('SpicePol IDE')
 ide.iconphoto(True, icon)
@@ -25,7 +27,7 @@ def set_file_path(path):
 
 def save_as():
     if file_path == '':
-        path = asksaveasfilename(filetypes = [('Python files', '*.py'), ('All files', '*.*')])
+        path = asksaveasfilename(filetypes = [('Python files', '*.py')])
     else:
         path = file_path    
     with open(path, 'w') as f:
@@ -34,7 +36,7 @@ def save_as():
         set_file_path(path)
 
 def open_file():
-    path = askopenfilename(filetypes = [('Python files', '*.py'), ('All files', '*.*')])
+    path = askopenfilename(filetypes = [('Python files', '*.py')])
     with open(path, 'r') as f:
         code = f.read()
         editor.delete('1.0', END)
@@ -59,22 +61,45 @@ def run():
 
 def bye():
     exit()
+    
 
-menu_bar = Menu(ide, background= background_color, fg= text_color)
+   
+    
+    
+    
+    
+
+menu_bar = Menu(ide, background= background_color_menu_bar, fg= text_color)
 
 #! FILE BAR
 file_bar = Menu(menu_bar, tearoff = 0, fg= text_color, background= background_color)
-file_bar.add_command(label = 'Open', command = open_file)
-file_bar.add_command(label = 'New')
-file_bar.add_command(label = 'Save', command = save_as)
-file_bar.add_command(label = 'Save As', command = save_as)
+file_bar.add_command(label = 'Open', command = open_file, background= background_color_2)
+file_bar.add_command(label = 'New', background= background_color_2)
+file_bar.add_command(label = 'Save', command = save_as, background= background_color_2)
+file_bar.add_command(label = 'Save As', command = save_as, background= background_color_2)
 menu_bar.add_cascade(label = 'File', menu = file_bar)
+
+#! EDIT BAR
+edit_bar = Menu(menu_bar, tearoff = 0, fg= text_color, background= background_color)
+edit_bar.add_command(label= 'Undo', background= background_color_2)
+edit_bar.add_command(label= 'Redo', background= background_color_2)
+edit_bar.add_command(label= 'Cut', background= background_color_2)
+edit_bar.add_command(label= 'Copy', background= background_color_2)
+edit_bar.add_command(label = 'Paste', background= background_color_2)
+edit_bar.add_command(label= 'Find', background= background_color_2)
+menu_bar.add_cascade(label = 'Edit', menu = edit_bar)
+
+
+
+
+
+
 
 
 #! RUN BAR
 run_bar = Menu(menu_bar, tearoff = 0, fg= text_color, background= background_color)
-run_bar.add_command(label = 'Run', command = run)
-run_bar.add_command(label = 'Exit', command = bye)
+run_bar.add_command(label = 'Run', command = run, background= background_color_2)
+run_bar.add_command(label = 'Exit', command = bye, background= background_color_2)
 menu_bar.add_cascade(label = 'Run', menu = run_bar)
 
 
